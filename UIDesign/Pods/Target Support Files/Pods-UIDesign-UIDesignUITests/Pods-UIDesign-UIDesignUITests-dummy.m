49338d655d6b8ffb2c4f7d27e5a357e0e5fa45c8
Daniyal Khan <dk720820@gmail.com>
2021-12-15 18:41:52 +0500

    Responsive Login and Signup screen

---
+++ UIDesign/Pods/Target Support Files/Pods-UIDesign-UIDesignUITests/Pods-UIDesign-UIDesignUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UIDesign_UIDesignUITests : NSObject
+@end
+@implementation PodsDummy_Pods_UIDesign_UIDesignUITests
+@end
---
+++ UIDesign/UIDesign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIDesign
+//
+//  Created by JOEYCO-0072PK on 15/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
